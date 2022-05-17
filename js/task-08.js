983a859b286d61438d7b405dc3b9eb9aadb1970c
Ruslan Karpov <karpovruslan1980@gmail.com>
2022-05-17 17:24:27 +0300

    task8-corrected

--- js/task-08.js
+++ js/task-08.js
@@ -1,50 +1,25 @@
-const form = document.querySelector(".login-form");
-form.addEventListener("submit", onFormSubmit);
+const form = document.querySelector('.login-form');
+const email = form.elements.email.value;
+const password = form.elements.password.value;
 
-function onFormSubmit(event) {
 
+
+const onFormSubmit = (event) => {
   event.preventDefault();
+  const {
+    elements: {
+      email: { value: email },
+      password: { value: password },
+    },
+  } = event.currentTarget;
 
-  console.log(event.currentTarget)
+  !email || !password
+    ? alert('Пожалуйста, заполните ВСЕ поля')
+    : console.log({ email: password });
 
-  const formEl = event.currentTarget.elements;
+  form.reset();
+};
 
-  if (formEl.email.value === "" || formEl.password.value === "") {
-    return alert(" Заповніть всі поля ");
-  }
-  const email = formEl.email.value;
-  const password = formEl.password.value;
-
-  const formData = {
-    email, password,
-  }
-  
-  console.log(`email: ${formEl.email.value}, Password: ${formEl.password.value}`);
-   
-  this.reset();
-};  
-//     const form = document.querySelector('.login-form');
-// const email = form.elements.email.value;
-// const password = form.elements.password.value;
-
-
-
-// const onFormSubmit = (event) => {
-// 	event.preventDefault();
-// const {
-// 		elements: {
-// 			email: { value: email },
-// 			password: { value: password },
-// 		},
-// 	} = event.currentTarget;
-
-// 	!email || !password
-// 		? alert('Пожалуйста, заполните ВСЕ поля')
-// 		: console.log({ email: password });
-
-// 	form.reset();
-// };
-
-// form.addEventListener('submit', onFormSubmit);
+form.addEventListener('submit', onFormSubmit);
     
 
